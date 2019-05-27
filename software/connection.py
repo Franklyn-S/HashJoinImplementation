f2672b159ad3e0b1d8102c9e8125b9fa4de76a77
Lukiam23 <mscorreia95@gmail.com>
2019-05-27 16:57:47 +0000

    [#1] Criação de arquivo txt

--- software/connection.py
+++ software/connection.py
@@ -6,7 +6,18 @@ cnxn = pyodbc.connect('Driver={SQL Server};'
                       'Database='+db_name+';'
                       'Trusted_Connection=yes;')
 cursor = cnxn.cursor()
-cursor.execute("SELECT * from dbo.part")	
+cursor.execute("SELECT * from dbo.nation")	
 
-for i in cursor:
-	print(i)
+arq = open('nation.txt', 'w')
+tupla = cursor.description
+colunas = []
+for i in tupla:
+	colunas.append(i[0])
+for linha in cursor:
+    string = ""
+    for index in range(0,len(colunas)):
+        string += str(linha[index])+","
+	
+    arq.write(string+"\n")
+arq.close()
+cursor.close()
